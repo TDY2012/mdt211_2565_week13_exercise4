329a6e1361a7ca7070cb231b60b94e3ed0af0a2c
Thitiwudh Uasmith <thitiwudh.u@mail.kmutt.ac.th>
2022-11-24 01:08:46 +0700

    feat: Added solution of week 13 exercise 4.

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static string ParsePostfixToInfix(Stack<char> postfix)
+    {
+        char parsingChar = postfix.Pop();
+        switch(parsingChar)
+        {
+            case '+':
+                return String.Format("({1}+{0})", ParsePostfixToInfix(postfix), ParsePostfixToInfix(postfix));
+                break;
+            case '-':
+                return String.Format("({1}-{0})", ParsePostfixToInfix(postfix), ParsePostfixToInfix(postfix));
+                break;
+            case '*':
+                return String.Format("({1}*{0})", ParsePostfixToInfix(postfix), ParsePostfixToInfix(postfix));
+                break;
+            case '/':
+                return String.Format("({1}/{0})", ParsePostfixToInfix(postfix), ParsePostfixToInfix(postfix));
+                break;
+            default:
+                return String.Format("{0}", parsingChar);
+                break;
+        }
+    }
+
+    static void InputPostfix(Stack<char> postfix)
+    {
+        char inputChar;
+        while(true)
+        {
+            inputChar = char.Parse(Console.ReadLine());
+            if(inputChar == '?')
+            {
+                break;
+            }
+            else
+            {
+                postfix.Push(inputChar);
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Stack<char> postfix = new Stack<char>();
+        InputPostfix(postfix);
+
+        string infix = ParsePostfixToInfix(postfix);
+        Console.WriteLine(infix);
+    }
+}
